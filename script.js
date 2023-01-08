4c4fb4114651e8461e9ab02c335624c3919dbd00
Alex Udodov <alex.udodov.web@gmail.com>
2023-01-08 19:52:16 +0200

    1024px layout

---
+++ script.js
@@ -0,0 +1,9 @@
+screenWidth = window.screen.width;
+
+
+// Adding breaks
+if (screenWidth >=1024) {
+    document.getElementById('inline-styled').innerHTML = 'В США такие уроки внедряются в школьную<br> программу как факультативы.';
+    document.getElementById('inc-title').innerHTML = 'Как проходит<br> обучение<br> на курсах INC';
+    
+}
